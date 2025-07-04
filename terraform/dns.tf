0e15023603888b65aca5c9b29b0c72cacf8fb92f
saikiranreven <rsksislvgy@gmail.com>
2025-07-04 21:17:51 -0230

    prevoius build

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -18,21 +18,3 @@ resource "google_dns_record_set" "failover_record" {
   managed_zone = google_dns_managed_zone.dr_zone.name
   rrdatas      = [google_compute_global_address.secondary_ip.address]  # Updated
 }
-
-resource "google_compute_health_check" "primary_health_check" {
-  name = "primary-region-hc"
-  http_health_check {
-    port         = 80
-    request_path = "/health"
-  }
-}
-
-resource "google_dns_policy" "auto_failover" {
-  name                      = "auto-failover-policy"
-  enable_logging            = true
-  networks { network_url = google_compute_network.vpc_primary.id }
-  
-  health_check_targets {
-    health_checks = [google_compute_health_check.primary_health_check.id]
-  }
-}
